3b359b4775f37fd2d8e3aebfb4877e10dbf28747
nurianc <nurhandrianc4@gmail.com>
2022-01-18 20:39:32 +0700

    tokohp v.1.0

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,9 +1,34 @@
 <div class="container-fluid">
-    <div class="row text-center">
+
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img src="<?php echo base_url('assets/img/slider1.jpg') ?>" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+            <img src="<?php echo base_url('assets/img/slider2.jpg') ?>" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-target="#carouselExampleCaptions" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-target="#carouselExampleCaptions" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </button>
+</div>
+
+    <div class="row text-center mt-3">
 
         <?php foreach($barang as $brg) :?>
 
-            <div class="card" style="width: 18rem;">
+            <div class="card ml-3" style="width: 18rem;">
   <img src="<?php echo base_url().'/uploads/'.$brg->gambar ?>" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title mb-1"><?php echo $brg->nama_brg ?></h5>
